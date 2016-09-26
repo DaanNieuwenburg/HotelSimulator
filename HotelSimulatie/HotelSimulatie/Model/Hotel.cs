1836de3af288ede49730f22467ddfdf2975c64b9
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-09-26 14:58:52 +0200

    Schoonmakers toegevoegd + lift is nu 1 lift

--- HotelSimulatie/HotelSimulatie/Model/Hotel.cs
+++ HotelSimulatie/HotelSimulatie/Model/Hotel.cs
@@ -11,9 +11,15 @@ namespace HotelSimulatie.Model
         public HotelRuimte[,] HotelLayout { get; set; }
         public Lobby LobbyRuimte { get; set; }
         public List<Gast> Gastenlijst { get; set; }
+        public Schoonmaker Schoonmaker_A { get; set; }
+        public Schoonmaker Schoonmaker_B { get; set; }
+        public Lift lift { get; set; }
         public Hotel()
         {
             Gastenlijst = new List<Gast>();
+            Schoonmaker_A = new Schoonmaker();
+            Schoonmaker_B = new Schoonmaker();
+            lift = new Lift();
             Addgasten();                        // tijdelijke testcode
 
 
@@ -24,44 +30,44 @@ namespace HotelSimulatie.Model
             HotelLayout[0, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[0, 2] = hotelRuimteFabriek.MaakHotelRuimte("Lobby");
             HotelLayout[0, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[0, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[0, 4] = lift;
 
             HotelLayout[1, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[1, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[1, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[1, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[1, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[1, 4] = lift;
 
             HotelLayout[2, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[2, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[2, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[2, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[2, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[2, 4] = lift;
 
 
             HotelLayout[3, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[3, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[3, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[3, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[3, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[3, 4] = lift;
 
             HotelLayout[4, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[4, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[4, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[4, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[4, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[4, 4] = lift;
 
             HotelLayout[5, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[5, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[5, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[5, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[5, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[5, 4] = lift;
 
             HotelLayout[6, 0] = hotelRuimteFabriek.MaakHotelRuimte("Trap");
             HotelLayout[6, 1] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[6, 2] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
             HotelLayout[6, 3] = hotelRuimteFabriek.MaakHotelRuimte("Kamer");
-            HotelLayout[6, 4] = hotelRuimteFabriek.MaakHotelRuimte("Lift");
+            HotelLayout[6, 4] = lift;
         }
         public void Addgasten()
         {
