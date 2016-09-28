7bf754629666ee27936bc8a53470bc4eecd50d32
roodkapje <nielssteensma@yahoo.nl>
2016-09-28 11:29:43 +0200

    Gast beweegt nu sneller maar flikkert op het einde

--- HotelSimulatie/HotelSimulatie/GeanimeerdeTexture.cs
+++ HotelSimulatie/HotelSimulatie/GeanimeerdeTexture.cs
@@ -21,34 +21,33 @@ namespace HotelSimulatie
             Texture = contentManager.Load<Texture2D>(textureNaam);
             TotaalAantalFrames = totaalAantalFrames;
             VerstrekenTijd = 0;
-            TijdPerFrame = 0.005f;
+            TijdPerFrame = 900;
         }
 
         public void UpdateFrame(GameTime spelTijd)
         {
-            VerstrekenTijd += (float)spelTijd.ElapsedGameTime.TotalSeconds;
-            if(VerstrekenTijd > TijdPerFrame)
+            VerstrekenTijd += spelTijd.ElapsedGameTime.Milliseconds;
+            Console.WriteLine("Verstrekentijd " + VerstrekenTijd);
+            if (VerstrekenTijd > TijdPerFrame)
             {
                 Frame++;
-                TijdPerFrame = TijdPerFrame + VerstrekenTijd;
-                VerstrekenTijd -= TijdPerFrame;
+                VerstrekenTijd = 0;
             }
             else
             {
                 // Reset het aantal frames naar 0, zodat er weer vanaf 0 geteld wordt.
                 if (Frame == TotaalAantalFrames)
                 {
-                    Frame = 0;
+                    Frame = 1;
                 }
             }
         }
 
         public void ToonFrame(SpriteBatch spriteBatch, Vector2 positie)
         {
-            int FrameGrootte = Texture.Width / 4;
+            int FrameGrootte = Texture.Width / TotaalAantalFrames;
             Rectangle sourcerect = new Rectangle(FrameGrootte * Frame, 0, FrameGrootte, Texture.Height);
             spriteBatch.Draw(Texture, positie, sourcerect, Color.White);
-            
         }
     }
 }
