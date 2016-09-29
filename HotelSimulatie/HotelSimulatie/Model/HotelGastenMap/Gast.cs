b3adb72c7d8d29f19a2ebb6b06f0eabea54671d0
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-09-29 10:26:38 +0200

    Lift plaatje toegevoegd

--- HotelSimulatie/HotelSimulatie/Model/HotelGastenMap/Gast.cs
+++ HotelSimulatie/HotelSimulatie/Model/HotelGastenMap/Gast.cs
@@ -49,7 +49,7 @@ namespace HotelSimulatie.Model
             }
             else
             {
-                positie = new Vector2(positie.X + 0.1f, positie.Y);
+                positie = new Vector2(positie.X + 0.2f, positie.Y);
             }
         }
 
