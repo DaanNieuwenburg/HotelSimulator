7a78d3d0aae58fcddc1e61dca72baed18d2312f3
Roodkapje <nielssteensma@yahoo.nl>
2016-10-24 14:26:30 +0200

    Fix voor meerdere in dezelfde kamer

--- HotelSimulatie/HotelSimulatie/Model/HotelRuimteMap/Lobby.cs
+++ HotelSimulatie/HotelSimulatie/Model/HotelRuimteMap/Lobby.cs
@@ -39,7 +39,7 @@ namespace HotelSimulatie.Model
                     // Zoekt een beschikbare kamer en bij geen ga telkens 1 ster omhoog
                     while (gast.ToegewezenKamer == null && gast.AantalSterrenKamer <= 5)
                     {
-                        List<Kamer> kamerz = hotel.hotelLayout.KamerLijst.FindAll(o => o.AantalSterren == gast.AantalSterrenKamer).ToList();
+                        List<Kamer> kamerz = hotel.hotelLayout.KamerLijst.FindAll(o => o.AantalSterren == gast.AantalSterrenKamer && o.Bezet == false).ToList();
                         List<List<HotelRuimte>> bestemmingenLijst = new List<List<HotelRuimte>>();
                         foreach (Kamer kamer in kamerz)
                         {
