459929619f80ebe5a225bcc8dbf72114450e9737
Roodkapje <nielssteensma@yahoo.nl>
2016-10-27 11:27:54 +0200

    Het trappenhuis is niet untracked :"(

---
+++ HotelSimulatie/HotelSimulatie/Model/HotelRuimteMap/Trappenhuis.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelSimulatie.Model
+{
+    public class Trappenhuis : HotelRuimte
+    {
+        public Trap trap { get; set; }
+        public Trappenhuis()
+        {
+            // Test
+            Naam = "Trap";
+            texturepath = @"Kamers\Trap_gesloten";
+        }
+        public override void LoadContent(ContentManager contentManager)
+        {
+            Texture = contentManager.Load<Texture2D>(texturepath);
+        }
+
+        public void VoegPersoonToe(Persoon persoon)
+        {
+            trap.VoegPersoonToe(persoon);
+        }
+    }
+}
